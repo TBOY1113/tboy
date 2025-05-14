7928a532ba21e7fc0e65e4bc4c6e56940b4ecf8a
TBOY <tharulbandara123@gmail.com>
2025-05-14 13:51:42 +0530

    Adding Files (Project)

---
+++ script.js
@@ -0,0 +1,44 @@
+//Work selector and work changer according to the selected category
+
+const workNames = document.querySelectorAll('.pg3 .wrapperPageThree .topseg .works  li');
+const progsInterfaceGraphic = document.getElementById('progsInterfaceGraphic');
+const progsInterfacePhotography = document.getElementById('progsInterfacePhotography');
+const progsInterfaceVideography = document.getElementById('progsInterfaceVideography');
+
+
+workNames.forEach((workName)=> {
+        workName.addEventListener('click', ()=>{
+            if (workName.id === 'graphicsWork'){
+                progsInterfaceGraphic.style.display = 'flex';
+                progsInterfacePhotography.style.display = 'none';
+                progsInterfaceVideography.style.display = 'none';
+                
+            }
+            else if (workName.id === 'photographyWork') {
+                progsInterfacePhotography.style.display = 'flex';
+                progsInterfaceVideography.style.display = 'none';
+                progsInterfaceGraphic.style.display = 'none';
+            }
+            else if (workName.id == 'videditWork'){
+                progsInterfaceVideography.style.display = 'flex';
+                progsInterfacePhotography.style.display = 'none';
+                progsInterfaceGraphic.style.display = 'none';
+            }
+        });
+});
+
+//sidebar
+
+const menubtn = document.getElementById('menubtn');
+const sideBar = document.getElementById('sideBar');
+const closebtn = document.getElementById('closebtn');
+
+menubtn.addEventListener('click', ()=> {
+
+    sideBar.style.display = 'flex';
+});
+
+closebtn.addEventListener('click', ()=> {
+
+    sideBar.style.display = 'none';
+});
